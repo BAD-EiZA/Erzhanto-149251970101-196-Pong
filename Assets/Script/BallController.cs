0276248602f322c9f7a32f858874112c8a8a3cb1
DESKTOP-BF1CS5G\EiZA <badapplestd@gmail.com>
2022-06-09 07:24:22 +0700

    Base Gameplay

--- Assets/Script/BallController.cs
+++ Assets/Script/BallController.cs
@@ -6,13 +6,42 @@ public class BallController : MonoBehaviour
 {
     public Vector2 spd;
     private Rigidbody2D rb;
+    //[Range(1f,40f)]
+    //public float degreeRange;
+    //public float initialForce;
 
     private void Start(){
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = spd;
+        RestartGame();
     }
     private void Update()
     {
         
+    }
+    void PushBall(){
+        rb.velocity = spd;
+        //float randomAngle = Random.Range(-degreeRange, degreeRange);
+        //.if (randomAngle == 0) {
+            //randomAngle += Random.Range(1f, 10f);
+        //}
+        //float randomRadian = randomAngle * Mathf.PI / 180;
+     
+        //float randomDirection = Random.Range(0, 2);
+
+        //float xInitialForce = initialForce * Mathf.Cos(randomRadian);
+        //float yInitialForce = initialForce * Mathf.Sin(randomRadian); 
+        //if (randomDirection < 1.0f) {
+            //xInitialForce = -xInitialForce;
+        //}
+        //rb.AddForce(new Vector2(xInitialForce, yInitialForce));
+    }
+    void ResetBall()
+    {
+        transform.position = Vector2.zero;
+        rb.velocity = Vector2.zero;
+    }
+    public void RestartGame(){
+        ResetBall();
+        Invoke("PushBall", 2);
     }
 }
